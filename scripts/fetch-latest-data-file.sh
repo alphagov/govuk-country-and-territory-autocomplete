a8817773aa5db5794cc23d6b4fc5679efc15f9d4
Caroline Green <caroline.green@digital.cabinet-office.gov.uk>
2017-07-06 15:12:20 +0100

    Write script for generating canonical lists

--- scripts/fetch-latest-data-file.sh
+++ scripts/fetch-latest-data-file.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
- wget -O dist/location-picker-graph.json https://5cq56v9u9f.execute-api.eu-west-2.amazonaws.com/prod/fetch
+wget -O dist/location-picker-graph.json https://5cq56v9u9f.execute-api.eu-west-2.amazonaws.com/prod/fetch
+node ./scripts/generate-canonical-list.js > dist/location-picker-canonical-list.json
