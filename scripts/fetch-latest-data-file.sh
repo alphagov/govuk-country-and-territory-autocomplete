d0e38bdecb84ce8e0c66c6bd0f662933c80af724
Theodor Vararu <theo@vararu.org>
2017-07-11 13:33:43 +0100

    Prettify canonical list and graph outputs
    This will allow us to get cleaner diffs that are easier to compare
    when updating the data.
    
    I pulled in https://www.npmjs.com/package/json-stable-stringify as a
    dependency because we needed a way to sort nested JSON objects.

--- scripts/fetch-latest-data-file.sh
+++ scripts/fetch-latest-data-file.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-wget -O dist/location-picker-graph.json https://5cq56v9u9f.execute-api.eu-west-2.amazonaws.com/prod/fetch
+wget -O tmp/location-picker-graph.json https://5cq56v9u9f.execute-api.eu-west-2.amazonaws.com/prod/fetch
 node ./scripts/generate-canonical-list.js > dist/location-picker-canonical-list.json
+node ./scripts/prettify-picker-graph.js > dist/location-picker-graph.json
