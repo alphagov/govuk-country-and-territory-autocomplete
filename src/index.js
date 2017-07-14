059a73ee9118034864ee25f143fa4751aabbf34d
Theodor Vararu <theo@vararu.org>
2017-07-14 10:11:12 +0100

    Add support for additional synonyms
    - Pass in `additionalSynonyms` argument to `openregister-picker-engine`
    - Pick up others from the list of options the progressively enhanced select

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function suggestionTemplate (result) {
 function openregisterLocationPicker (opts) {
   // Set defaults.
   opts.fallback = opts.fallback || ((query, syncResults) => {
-    const availableOptions = Array.prototype.map.call(opts.selectElement.options, o => o.innerHTML)
+    const availableOptions = Array.prototype.map.call(opts.selectElement.options, o => o.innerText)
     const filteredResults = query
       ? availableOptions.filter(r => r.toLowerCase().indexOf(query.toLowerCase()) !== -1)
       : []
@@ -29,8 +29,24 @@ function openregisterLocationPicker (opts) {
     var requestedOption = Array.prototype.filter.call(opts.selectElement.options, o => o.innerText === (result && result.name))[0]
     if (requestedOption) { requestedOption.selected = true }
   })
+  
+  const optionsWithAdditionalSynonyms = Array.prototype
+    .filter.call(opts.selectElement.options, option => option.dataset && option.dataset.additionalSynonyms)
+  const htmlAdditionalSynonyms = optionsWithAdditionalSynonyms
+    .map(option => ({ code: option.value, synonyms: option.dataset.additionalSynonyms }))
+    .reduce((allSynonyms, currentSet) => {
+      const moreSynonyms = JSON.parse(currentSet.synonyms)
+        .map(synonymName => ({ name: synonymName, code: currentSet.code }))
+      return allSynonyms.concat(moreSynonyms)
+    }, [])
 
-  opts.source = opts.source || openregisterPickerEngine({ url: opts.url, fallback: opts.fallback })
+  opts.additionalSynonyms = (opts.additionalSynonyms || []).concat(htmlAdditionalSynonyms)
+
+  opts.source = opts.source || openregisterPickerEngine({
+    additionalSynonyms: opts.additionalSynonyms,
+    url: opts.url,
+    fallback: opts.fallback
+  })
 
   opts.templates = opts.templates || {
     inputValue: inputValueTemplate,
